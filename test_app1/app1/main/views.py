7971034a296afe97ffa22267a24637c8c3393474
jegorix <0311egor@gmail.com>
2025-04-10 21:33:48 +0300

    create new app -> news

--- test_app1/app1/main/views.py
+++ test_app1/app1/main/views.py
@@ -2,7 +2,19 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, 'main/index.html')
+
+    data = {
+        'title': 'Главная страницa',
+        'values': ['Menu', 'Categories', 'Traceback'],
+
+        'obj': {
+            'car': 'bmw',
+            'age': 18,
+            'hobby': 'football',
+        }
+    }
+
+    return render(request, 'main/index.html',  data)
 
 def about(request):
     return render(request, 'main/about.html')
