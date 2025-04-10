395389c77f11577339fde8ae34ce77cce26f79de
jegorix <0311egor@gmail.com>
2025-04-11 01:17:15 +0300

    operate form -> connect to models -> save to db

--- test_app1/app1/news/views.py
+++ test_app1/app1/news/views.py
@@ -1,10 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Articles
+from .forms import ArticlesForm
 
 # Create your views here.
 def news_home(request):
     news = Articles.objects.order_by('-date')
     return render(request, 'news/news_home.html', {'news': news})
 
+
+
+
 def create(request):
-    return render(request, 'news/create.html')
+    error = ''
+    if request.method == 'POST':
+        form = ArticlesForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+        else:
+            error = 'Форма заполнена неверно'
+
+
+
+
+
+
+
+    form = ArticlesForm()
+    data = {
+        'form': form,
+        'error': error
+    }
+    return render(request, 'news/create.html', {'form': form})
