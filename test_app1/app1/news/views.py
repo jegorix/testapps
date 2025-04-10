464458b02ae10321c610aa3f9eda8315d7b5883a
jegorix <0311egor@gmail.com>
2025-04-11 00:20:26 +0300

    create form

--- test_app1/app1/news/views.py
+++ test_app1/app1/news/views.py
@@ -5,3 +5,6 @@ from .models import Articles
 def news_home(request):
     news = Articles.objects.order_by('-date')
     return render(request, 'news/news_home.html', {'news': news})
+
+def create(request):
+    return render(request, 'news/create.html')
