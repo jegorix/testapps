d8e016f931963e330ab6105b2bd0bdf8617d0ed2
jegorix <0311egor@gmail.com>
2025-04-12 21:52:39 +0300

    create first model Task and register in admin

--- test_app2/app2/main/models.py
+++ test_app2/app2/main/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Tasks(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateField(blank=True, null=True)
+    is_done = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Задача'
+        verbose_name_plural = 'Задачи'
