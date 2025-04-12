361e1ebc961463d5ce51ba44f0f65644af35c36d
jegorix <0311egor@gmail.com>
2025-04-12 12:47:24 +0300

    add url: news/id (for details) and replaced func news_home with NewsHome class

--- test_app1/app1/news/urls.py
+++ test_app1/app1/news/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.news_home, name='news_home'),
+    # path('', views.news_home, name='news_home'),
+    path('', views.NewsHome.as_view(), name='news_home'),
     path('create/', views.create, name = 'create'),
+    path('<int:pk>', views.NewsDetailView.as_view(), name='news_detail')
 ]
