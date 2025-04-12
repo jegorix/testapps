1b2b1ee4dbb404cae168a6a567d57e5018b37cdb
jegorix <0311egor@gmail.com>
2025-04-13 01:15:29 +0300

    handle show and create func

--- test_app2/app2/main/views.py
+++ test_app2/app2/main/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import CreateView
 
 # Create your views here.
+
+def create(request):
+    return render(request, 'main/create.html')
+
+def show(request):
+    return render(request, 'main/create.html')
+
