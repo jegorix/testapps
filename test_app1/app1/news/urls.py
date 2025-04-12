0baf6aadb3cd8840884be169d018c8dd21262064
jegorix <0311egor@gmail.com>
2025-04-12 16:47:48 +0300

    replace func create with class CreateView

--- test_app1/app1/news/urls.py
+++ test_app1/app1/news/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     # path('', views.news_home, name='news_home'),
     path('', views.NewsHome.as_view(), name='news_home'),
-    path('create/', views.create, name = 'create'),
+    path('create/', views.ArticleCreateView.as_view(), name = 'create'),
     path('<int:pk>', views.NewsDetailView.as_view(), name='news_detail'),
     path('<int:pk>/update/',views.NewsUpdateView.as_view(), name='news_update'),
     path('<int:pk>/delete/', views.NewsDeleteView.as_view(), name='news_delete'),
