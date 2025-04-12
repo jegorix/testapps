4de2e9e524c4bdd10d76c1193b81b5b33ab1344f
jegorix <0311egor@gmail.com>
2025-04-12 12:47:53 +0300

    add url: news/id (for details) and replaced func news_home with NewsHome class

--- test_app1/app1/news/views.py
+++ test_app1/app1/news/views.py
@@ -1,12 +1,26 @@
 from django.shortcuts import render, redirect
 from .models import Articles
 from .forms import ArticlesForm
+from django.views.generic import DetailView, ListView
 
 # Create your views here.
-def news_home(request):
-    news = Articles.objects.order_by('-date')
-    return render(request, 'news/news_home.html', {'news': news})
 
+# def news_home(request):
+#     news = Articles.objects.order_by('-date')
+#     return render(request, 'news/news_home.html', {'news': news})
+
+class NewsHome(ListView):
+    model = Articles
+    template_name = 'news/news_home.html'
+    context_object_name = 'news'
+    ordering = ['-date']
+
+
+
+class NewsDetailView(DetailView):
+    model = Articles
+    template_name = 'news/detail_news.html'
+    context_object_name = 'article'
 
 
 
@@ -19,11 +33,6 @@ def create(request):
             return redirect('index')
         else:
             error = 'Форма заполнена неверно'
-
-
-
-
-
 
 
     form = ArticlesForm()
