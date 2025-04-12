845aa3c93efe01dac6b250989ef5d39c45ef5cda
jegorix <0311egor@gmail.com>
2025-04-12 16:20:03 +0300

    create update and delete class

--- test_app1/app1/news/views.py
+++ test_app1/app1/news/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import Articles
 from .forms import ArticlesForm
-from django.views.generic import DetailView, ListView
+from django.views.generic import DetailView, ListView, UpdateView, DeleteView
 
 # Create your views here.
 
@@ -21,6 +21,23 @@ class NewsDetailView(DetailView):
     model = Articles
     template_name = 'news/detail_news.html'
     context_object_name = 'article'
+
+
+class NewsUpdateView(UpdateView):
+    model = Articles
+    template_name = 'news/create.html'
+
+    form_class = ArticlesForm
+
+class NewsDeleteView(DeleteView):
+    model = Articles
+    template_name = "news/news_delete.html"
+    success_url = '/news/'
+    context_object_name = 'article'
+
+
+
+
 
 
 
