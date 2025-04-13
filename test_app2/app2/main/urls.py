c92266a4ba28af54a2c310df418b23a61dea40a3
jegorix <0311egor@gmail.com>
2025-04-13 14:15:49 +0300

    create functional form to add task and flush message

--- test_app2/app2/main/urls.py
+++ test_app2/app2/main/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    # path('', views.TaskCreateView.as_view(), name='create_task'),
-    path('create/', views.create, name='create_task'),
+    path('', views.TaskCreateView.as_view(), name='create_task'),
+    # path('create/', views.create, name='create_task'),
     path('show/', views.show, name='show_tasks'),
 ]
--- test_app2/app2/main/views.py
+++ test_app2/app2/main/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
+from .models import Tasks
+from .forms import TasksForm
+from django.contrib import messages
 from django.views.generic import CreateView
 
 # Create your views here.
+#
+class TaskCreateView(CreateView):
+    model = Tasks
+    template_name = 'main/create.html'
+    form_class = TasksForm
+    success_url = '/tmanager/show/'
 
-def create(request):
-    return render(request, 'main/create.html')
+    def form_valid(self, form):
+        messages.success(self.request, 'Task Created!')
+        return super().form_valid(form)
+
 
 def show(request):
     return render(request, 'main/create.html')
