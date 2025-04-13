22084bc793065d82796910e3b1cf87faaff6b8c8
jegorix <0311egor@gmail.com>
2025-04-13 14:16:43 +0300

    extends tasks model by priority(radio select)

--- test_app2/app2/main/models.py
+++ test_app2/app2/main/models.py
@@ -3,11 +3,25 @@ from django.db import models
 # Create your models here.
 
 class Tasks(models.Model):
+
+    PRIORITY_CHOICES = [
+        ('low', 'Low'),
+        ('medium', 'Medium'),
+        ('high', 'High'),
+
+    ]
+
     title = models.CharField(max_length=100)
     description = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     deadline = models.DateField(blank=True, null=True)
     is_done = models.BooleanField(default=False)
+
+    priority = models.CharField(
+        max_length=10,
+        choices=PRIORITY_CHOICES,
+        default='medium',
+    )
 
     def __str__(self):
         return self.title
