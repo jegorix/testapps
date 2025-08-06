625c56511d075e4c4db53e46e9105740d47706fc
jegorix <0311egor@gmail.com>
2025-08-06 15:59:09 +0300

    Implement share posts by e-mail

--- stepik-django/blog-1/mysite/blog/views.py
+++ stepik-django/blog-1/mysite/blog/views.py
@@ -2,8 +2,18 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.http import Http404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
+from .forms import EmailPostForm
+from django.core.mail import send_mail
+from django.conf import settings
 
 # Create your views here.
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
 
 
 def post_list(request):
@@ -42,5 +52,43 @@ def post_detail(request, year, month, day, post):
                   context={"post": post})
 
 
+
+
+def post_share(request, post_id):
+    post = get_object_or_404(Post,
+                             id=post_id,
+                             status=Post.Status.PUBLISHED)
+    
+    sent = False
+    
+    if request.method == "POST":
+        form = EmailPostForm(request.POST)  
+        if form.is_valid():
+            
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            
+            subject = f"{cd["name"]} recommends you read {post.title}"
+            
+            message = f"Read {post.title} at {post_url}\n\n" \
+                f"{cd["name"]}'s ({cd['email']}) comments: {cd["comments"]}"
+                
+            send_mail(subject, message, settings.EMAIL_HOST_USER,
+                      [cd['to']])
+            
+            sent = True
+            
+            
+
+    else:
+        form = EmailPostForm()
+    
+    return render(request=request,
+                  template_name='blog/post/share.html',
+                  context={
+                      "post": post,
+                      "form": form,
+                      "sent": sent
+                  })
 
 
