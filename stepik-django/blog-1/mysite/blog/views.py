c92c4aa82e38b9e481ba89aecbfdccc2349c57a4
jegorix <0311egor@gmail.com>
2025-08-09 16:00:14 +0300

    Created a basic search engine for the blog

--- stepik-django/blog-1/mysite/blog/views.py
+++ stepik-django/blog-1/mysite/blog/views.py
@@ -3,12 +3,16 @@ from .models import Post, Comment
 from django.http import Http404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
-from .forms import EmailPostForm, CommentForm
+
+from .forms import EmailPostForm, CommentForm, SearchForm
 from django.core.mail import send_mail
 from django.conf import settings
 from django.views.decorators.http import require_POST
 from taggit.models import Tag
 from django.db.models import Count
+
+from django.contrib.postgres.search import SearchVector
+
 # Create your views here.
 
 class PostListView(ListView):
@@ -103,8 +107,6 @@ def post_share(request, post_id):
             
             sent = True
             
-            
-
     else:
         form = EmailPostForm()
     
@@ -135,3 +137,23 @@ def post_comment(request, post_id):
                            "form": form,
                            "comment": comment})
     
+    
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+    
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            
+            results = Post.published.annotate(
+                search=SearchVector('title', 'body'),
+            ).filter(search=query)
+            
+    return render(request,
+                  'blog/post/search.html',
+                  {'form': form,
+                   'query': query,
+                   'results': results})
--- stepik-django/blog-1/mysite/mysite/settings.py
+++ stepik-django/blog-1/mysite/mysite/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-import ssl
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -43,7 +42,8 @@ INSTALLED_APPS = [
     'blog.apps.BlogConfig',
     'taggit',
     'django.contrib.sites',
-    'django.contrib.sitemaps'
+    'django.contrib.sitemaps',
+    'django.contrib.postgres'
 ]
 
 MIDDLEWARE = [
