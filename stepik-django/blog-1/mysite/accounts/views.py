38d913d4be8559911f90fc9d72ce5266e78dd1ef
jegorix <0311egor@gmail.com>
2025-08-13 17:07:14 +0300

    Improvements to the authorization and registration system, adding sessions

--- stepik-django/blog-1/mysite/accounts/views.py
+++ stepik-django/blog-1/mysite/accounts/views.py
@@ -1,9 +1,52 @@
-from .forms import SignUpForm
+from .forms import SignUpForm, LoginForm
 from django.urls import reverse_lazy
 from django.views import generic
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.views import LoginView
+
 
 # Create your views here.
 class SignUpView(generic.CreateView):
     form_class = SignUpForm
     success_url = reverse_lazy("login")
+    initial = None # принимает {'key': 'value'}
     template_name = "registration/signup.html"
+    
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect(to='/')
+        
+        return super(SignUpView, self).dispatch(request, *args, **kwargs)
+    
+    def get(self, request, *args, **kwargs):
+        form = self.form_class(initial=self.initial)
+        return render(request, self.template_name, context={'form': form})
+    
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
+        
+        if form.is_valid():
+            form.save()
+            
+            username = form.cleaned_data.get("username")
+            messages.success(request, f"Account created for {username}")
+            
+            return redirect(to='login')
+    
+        return render(request, self.template_name, context={'form': form})
+    
+    
+
+class CustomLoginView(LoginView):
+    form_class = LoginForm
+    
+    def form_valid(self, form):
+        remember_me = form.cleaned_data.get("remember_me")
+        
+        if not remember_me:
+            self.request.session.set_expiry(0)
+            self.request.session.modified = True
+            
+        return super(CustomLoginView, self).form_valid(form)
+    
