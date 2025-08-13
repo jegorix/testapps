ed720aa48322d2e6bdc9870bb7be778f12872093
jegorix <0311egor@gmail.com>
2025-08-13 15:50:36 +0300

    Fix user register logic and error styles

--- stepik-django/blog-1/mysite/accounts/forms.py
+++ stepik-django/blog-1/mysite/accounts/forms.py
@@ -3,9 +3,14 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
 class SignUpForm(UserCreationForm):
+    first_name = forms.CharField(max_length=100)
+    last_name = forms.CharField(max_length=100)
     username = forms.CharField(max_length=20)
-    email = forms.EmailField(max_length=200)
+    email = forms.EmailField()
+    password1 = forms.CharField(max_length=50, widget=forms.PasswordInput())
+    password2 = forms.CharField(max_length=50, widget=forms.PasswordInput())
     
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        fields = ['first_name', 'last_name', 'username', 'email', 'password1', 'password2']
+        
